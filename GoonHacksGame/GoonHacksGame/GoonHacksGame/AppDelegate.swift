a415843d690db98ca040d0d9a9aa751af9320f0d
Jacob Mobin <jacobamobin@gmail.com>
2025-11-08 12:20:56 -0500

    Add xcode project

---
+++ GoonHacksGame/GoonHacksGame/GoonHacksGame/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  GoonHacksGame
+//
+//  Created by Jacob Mobin on 11/8/25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
